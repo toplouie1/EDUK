5856117760167fdad3a5da3fe3c632ccbd782562
Nima sherpa <Toplouie78@gmail.com>
2024-05-13 23:30:33 -0400

    error handler for metors endpoint

--- backend/controllers/mentorsControllers.js
+++ backend/controllers/mentorsControllers.js
@@ -5,99 +5,120 @@ const mentors = express.Router();
 const upload = require("../multer.js");
 
 const {
-  getAllMentors,
-  getOneMentor,
-  createMentor,
-  updateMentor,
-  deleteMentor,
+	getAllMentors,
+	getOneMentor,
+	createMentor,
+	updateMentor,
+	deleteMentor,
 } = require("../queries/mentors.js");
+const { isIDValid } = require("./helper.js");
 
 // Get all mentors
 mentors.get("/", async (req, res) => {
-  const allMentors = await getAllMentors();
-  if (allMentors[0]) {
-    res.json({ success: true, result: allMentors });
-  } else {
-    res.status(500).json({ success: false, error: "server error..." });
-  }
+	try {
+		const allMentors = await getAllMentors();
+		if (allMentors[0]) {
+			res.json({ success: true, result: allMentors });
+		}
+	} catch (err) {
+		res.status(500).json({ success: false, error: err });
+	}
 });
 
 // Get one mentor
 mentors.get("/:mid", async (req, res) => {
-  const { mid } = req.params;
-  const mentor = await getOneMentor(mid);
-  if (mentor.mentor_id) {
-    res.json({ success: true, result: mentor });
-  } else {
-    res.status(500).json({
-      success: false,
-      error: `server error, no mentor at index ${mid}`,
-    });
-  }
+	try {
+		const { mid } = req.params;
+		if (!isIDValid(mid)) {
+			return res.status(404).json({ error: "the id is not valid" });
+		}
+		const mentor = await getOneMentor(mid);
+		if (mentor.mentor_id) {
+			res.json({ success: true, result: mentor });
+		}
+	} catch (err) {
+		res.status(500).json({
+			success: false,
+			error: `server error, no mentor at index ${mid}`,
+		});
+	}
 });
 
 // Create a mentor
 mentors.post("/", async (req, res) => {
-  const mentor = req.body;
-  const createdMentor = await createMentor(mentor);
-  if (createdMentor.mentor_id) {
-    res.json({ success: true, result: mentor });
-  } else {
-    res
-      .status(500)
-      .json({ success: false, error: "unable to create mentor..." });
-  }
+	const mentor = req.body;
+	const createdMentor = await createMentor(mentor);
+	if (createdMentor.mentor_id) {
+		res.json({ success: true, result: mentor });
+	} else {
+		res
+			.status(500)
+			.json({ success: false, error: "unable to create mentor..." });
+	}
 });
 //update mentor image
 
 mentors.post("/:mentor_id/upload", upload.single("photo"), async (req, res) => {
-  const { mentor_id } = req.params;
+	const { mentor_id } = req.params;
 
-  const file = req.file;
+	const file = req.file;
 
-  const mentor = await getOneMentor(mentor_id);
+	const mentor = await getOneMentor(mentor_id);
 
-  if (mentor.mentor_id) {
-    mentor.mentor_image = file.filename;
+	if (mentor.mentor_id) {
+		mentor.mentor_image = file.filename;
 
-    var updatedMentor = await updateMentor(mentor_id, mentor);
-  }
+		var updatedMentor = await updateMentor(mentor_id, mentor);
+	}
 
-  try {
-    if (updatedMentor.mentor_id)
-      return res.json({
-        success: true,
-      });
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		if (!isIDValid(mid)) {
+			return res.status(404).json({ error: "the id is not valid" });
+		}
+		if (updatedMentor.mentor_id)
+			return res.json({
+				success: true,
+			});
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 // Update a mentor
 mentors.put("/:mid", async (req, res) => {
-  const mentor = req.body;
-  const { mid } = req.params;
-  const updatedMentor = await updateMentor(mid, mentor);
-  if (updatedMentor.mentor_id) {
-    res.json({ success: true, result: updatedMentor });
-  } else {
-    res
-      .status(500)
-      .json({ success: false, error: `unable to update mentor ${mid}` });
-  }
+	const mentor = req.body;
+	const { mid } = req.params;
+	const updatedMentor = await updateMentor(mid, mentor);
+	try {
+		if (!isIDValid(mid)) {
+			return res.status(404).json({ error: "the id is not valid" });
+		}
+		if (updatedMentor.mentor_id) {
+			res.json({ success: true, result: updatedMentor });
+		}
+	} catch (err) {
+		res
+			.status(500)
+			.json({ success: false, error: `unable to update mentor ${mid}` });
+	}
 });
 
 // Delete a mentor
 mentors.delete("/:mid", async (req, res) => {
-  const { mid } = req.params;
-  const deletedMentor = await deleteMentor(mid);
-  if (deletedMentor.mentor_id) {
-    res.json({ success: true, result: deletedMentor });
-  } else {
-    res
-      .status(500)
-      .json({ success: false, error: `unable to delete mentor ${mid}` });
-  }
+	const { mid } = req.params;
+	const deletedMentor = await deleteMentor(mid);
+	try {
+		if (!isIDValid(mid)) {
+			return res.status(404).json({ error: "the id is not valid" });
+		}
+		if (deletedMentor.mentor_id) {
+			res.json({ success: true, result: deletedMentor });
+		}
+	} catch (err) {
+		res
+			.status(500)
+			.json({ success: false, error: `unable to delete mentor ${mid}` });
+	}
 });
 
 module.exports = mentors;
