67edad6ed8bb5dad99955f2689a5af8a0693ca2f
Nima sherpa <Toplouie78@gmail.com>
2024-05-29 11:12:03 -0400

    adding a toggle for sorting by id as default and with Name

--- frontend/src/components/Resources.js
+++ frontend/src/components/Resources.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import axios from "axios";
 import { useState, useEffect } from "react";
 import SingleResource from "./SingleResource";
@@ -8,6 +7,15 @@ const API = process.env.REACT_APP_API_URL;
 
 export default function Resources() {
 	const [resources, setResources] = useState([]);
+	const [sortKey, setSortKey] = useState("resource_id");
+	const [ascending, setAscending] = useState(true);
+
+	const handleSortKeyChange = (e) => {
+		setSortKey(e.target.value);
+	};
+	const toggleButton = () => {
+		setAscending(!ascending);
+	};
 
 	useEffect(() => {
 		axios
@@ -17,19 +25,42 @@ export default function Resources() {
 					(resource) => resource.is_verified
 				);
 				setResources(allResources);
+				console.log(allResources);
 			})
 			.catch((error) => {
 				console.log(error);
 			});
 	}, []);
 
+	const resourcesToDisplay = resources.sort((a, b) => {
+		if (a[sortKey] < b[sortKey]) {
+			return ascending ? -1 : 1;
+		} else if (a[sortKey] > b[sortKey]) {
+			return ascending ? 1 : -1;
+		} else {
+			return 0;
+		}
+	});
+
+	const renderContent = () => {
+		return resourcesToDisplay.map((resource) => (
+			<SingleResource key={resource.resource_id} resource={resource} />
+		));
+	};
+
 	return (
-		<div className="resource-arr">
-			{resources.map((resource) => {
-				return (
-					<SingleResource key={resource.resource_id} resource={resource} />
-				);
-			})}
+		<div>
+			<section>
+				<div>
+					<label>Sort by: </label>
+					<select value={sortKey} onChange={handleSortKeyChange}>
+						<option value="resource_id">default</option>
+						<option value="resource_name">Name</option>
+					</select>
+				</div>
+				<button onClick={toggleButton}>{ascending ? "⬇️" : "⬆️"}</button>
+			</section>
+			<div className="resource-arr">{renderContent()}</div>
 		</div>
 	);
 }
